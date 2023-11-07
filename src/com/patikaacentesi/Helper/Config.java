8d9cec044d4d2d7eec0fab0c5cae4c66ecbe242d
CISA <cahitisaoglu10@gmail.com>
2023-11-07 15:41:56 +0300

    fix

--- src/com/patikaacentesi/Helper/Config.java
+++ src/com/patikaacentesi/Helper/Config.java
@@ -2,8 +2,8 @@ package com.patikaacentesi.Helper;
 
 public class Config {
     // DEĞERLENDİRME FORMU 6
-    public static final String PROJECT_TITLE= "PATİKA ACENTESİ";
+    public static final String PROJECT_TITLE= "XXX";
     public static final String DB_URL= "jdbc:mysql://localhost/patikaacente";
-    public static final String DB_USERNAME = "root";
-    public static final String DB_PASSWORD = "mysql";
+    public static final String DB_USERNAME = "XXXX";
+    public static final String DB_PASSWORD = "XXXX";
 }
